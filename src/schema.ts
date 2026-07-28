175d6fc17102b6265b44a797d3246cc8f9ef9cf9
Huzaifah Goher <huzaifah191001@gmail.com>
2026-07-28 21:37:47 +0500

    - Added marks for bold, italic, underline and added their shotcut keys as well in keymap.ts

--- src/schema.ts
+++ src/schema.ts
@@ -1,34 +1,57 @@
-import { DOMOutputSpec, Node, NodeSpec, Schema } from "prosemirror-model";
+import {
+  DOMOutputSpec,
+  MarkSpec,
+  Node,
+  NodeSpec,
+  Schema,
+} from "prosemirror-model";
 
-const returnParagraphElement = (node: Node) : DOMOutputSpec => {
-    const p = document.createElement("p");
-    p.setAttribute("linespacing", node?.attrs?.lineSpacing || "one");
-    return {dom: p, contentDOM: p};
-}
-
-const pDom: DOMOutputSpec = ["p", 0];
+const returnParagraphElement = (node: Node): DOMOutputSpec => {
+  const p = document.createElement("p");
+  p.setAttribute("linespacing", node?.attrs?.lineSpacing || "one");
+  return { dom: p, contentDOM: p };
+};
 
 const nodes = {
-    doc: {
-        content: "block+"
-    } as NodeSpec,
-    paragraph: {
-        content: "inline*",
-        group: "block",
-        attrs: {
-            lineSpacing: {default: "double", validate: "string"}
-        },
-        parseDOM: [{tag: "p"}],
-        toDOM: (node: Node) => {
-            const pElement = returnParagraphElement(node);
-            return pElement;
-        },
-    } as NodeSpec,
-    text: {
-        group: "inline"
-    } as NodeSpec
-}
+  doc: {
+    content: "block+",
+  } as NodeSpec,
+  paragraph: {
+    content: "inline*",
+    group: "block",
+    attrs: {
+      lineSpacing: { default: "double", validate: "string" },
+    },
+    parseDOM: [{ tag: "p" }],
+    toDOM: (node: Node) => {
+      const pElement = returnParagraphElement(node);
+      return pElement;
+    },
+  } as NodeSpec,
+  text: {
+    group: "inline",
+  } as NodeSpec,
+};
 
-const marks = {};
+const marks = {
+  strong: {
+    toDOM() {
+      return ["strong", 0];
+    },
+    parseDOM: [{ tag: "strong" }, { tag: "b" }, { style: "font-weight=bold" }],
+  } as MarkSpec,
+  em: {
+    toDOM() {
+      return ["em", 0];
+    },
+    parseDOM: [{ tag: "i" }, {tag: "em"}, {style: "font-style=italic"}],
+  } as MarkSpec,
+  underline: {
+    toDOM() {
+      return ["u", 0];
+    },
+    parseDOM: [{ tag: "u" }, {style: "text-decoration=underline"}],
+  } as MarkSpec,
+};
 
-export const schema = new Schema({nodes, marks});
+export const schema = new Schema({ nodes, marks });
