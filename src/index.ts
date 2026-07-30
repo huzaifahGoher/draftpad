9517c6db95c5f075752a6a07f3fd3a147255f04c
Huzaifah Goher <huzaifah191001@gmail.com>
2026-07-30 19:50:46 +0500

    Added buttons for basic toolbar and added listeners for their click event.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { EditorView } from "prosemirror-view";
 import { history, undo, redo } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
 import { customKeyMap } from "./keymap";
+import { toggleMark } from "prosemirror-commands";
 
 const editor = EditorState.create({
   schema,
@@ -18,4 +19,19 @@ const view = new EditorView(element, {
     const newState = view.state.apply(transaction);
     view.updateState(newState);
   },
+});
+
+document.getElementById("btn-bold")?.addEventListener("click", ()=>{
+  toggleMark(schema.marks.strong)(view.state, view.dispatch);
+  view.focus();
+});
+
+document.getElementById("btn-italic")?.addEventListener("click", ()=>{
+  toggleMark(schema.marks.em)(view.state, view.dispatch);
+  view.focus();
+});
+
+document.getElementById("btn-underline")?.addEventListener("click", ()=>{
+  toggleMark(schema.marks.underline)(view.state, view.dispatch);
+  view.focus();
 });
