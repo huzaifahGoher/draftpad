b0eefa577eb778e01052ad406d970e149ffe8017
Huzaifah Goher <huzaifah191001@gmail.com>
2026-07-30 19:42:56 +0500

    Added headings nodes and their keympas

--- src/keymap.ts
+++ src/keymap.ts
@@ -1,4 +1,4 @@
-import { splitBlock, deleteSelection, joinBackward, chainCommands, toggleMark } from "prosemirror-commands";
+import { splitBlock, deleteSelection, joinBackward, chainCommands, toggleMark, setBlockType } from "prosemirror-commands";
 import { EditorView } from "prosemirror-view";
 import { EditorState, Transaction } from "prosemirror-state";
 import { schema } from "./schema";
@@ -11,4 +11,11 @@ export const customKeyMap = {
     "Ctrl-b": toggleMark(schema.marks.strong),
     "Ctrl-i": toggleMark(schema.marks.em),
     "Ctrl-u": toggleMark(schema.marks.underline),
+    "Alt-1": setBlockType(schema.nodes.heading, {level: 1}),
+    "Alt-2": setBlockType(schema.nodes.heading, {level: 2}),
+    "Alt-3": setBlockType(schema.nodes.heading, {level: 3}),
+    "Alt-4": setBlockType(schema.nodes.heading, {level: 4}),
+    "Alt-5": setBlockType(schema.nodes.heading, {level: 5}),
+    "Alt-6": setBlockType(schema.nodes.heading, {level: 6}),
+    "Alt-0": setBlockType(schema.nodes.paragraph),
 }
--- src/schema.ts
+++ src/schema.ts
@@ -28,6 +28,24 @@ const nodes = {
       return pElement;
     },
   } as NodeSpec,
+  heading: {
+    content: "inline*",
+    group: "block",
+    attrs: {
+      level: { default: 1 },
+    },
+    parseDOM: [
+      { tag: "h1", attrs: { level: 1 } },
+      { tag: "h2", attrs: { level: 2 } },
+      { tag: "h3", attrs: { level: 3 } },
+      { tag: "h4", attrs: { level: 4 } },
+      { tag: "h5", attrs: { level: 5 } },
+      { tag: "h6", attrs: { level: 6 } },
+    ],
+    toDOM: (node:Node) => {
+      return [`h${node.attrs.level}`, 0];
+    },
+  } as NodeSpec,
   text: {
     group: "inline",
   } as NodeSpec,
@@ -44,13 +62,13 @@ const marks = {
     toDOM() {
       return ["em", 0];
     },
-    parseDOM: [{ tag: "i" }, {tag: "em"}, {style: "font-style=italic"}],
+    parseDOM: [{ tag: "i" }, { tag: "em" }, { style: "font-style=italic" }],
   } as MarkSpec,
   underline: {
     toDOM() {
       return ["u", 0];
     },
-    parseDOM: [{ tag: "u" }, {style: "text-decoration=underline"}],
+    parseDOM: [{ tag: "u" }, { style: "text-decoration=underline" }],
   } as MarkSpec,
 };
 
